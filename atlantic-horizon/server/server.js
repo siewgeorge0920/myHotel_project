061fe98044f810b9b78991334efe4bb10351f4b0
lincolnteo <lincontmk@gmail.com>
2026-03-15 15:38:15 +0000

    Manual Merge and

--- atlantic-horizon/server/server.js
+++ atlantic-horizon/server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -10,8 +12,16 @@ app.post("/api/newsletter", (req, res) => {
   console.log("Newsletter signup:", firstName, email);
   res.json({ success: true, message: "Subscribed!" });
 });
+const PORT = process.env.PORT || 5000;
 
-const PORT = 5000;
+// 🚀 连接 MONGODB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Fuiyoh! 🚀 MongoDB Atlas 连接成功!"))
+  .catch((error) =>
+    console.log("Alamak, Database 连线失败 ❌:", error.message),
+  );
+  
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("✅ Backend server tengah run kat port:" + PORT);
 });
