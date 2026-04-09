be60920da7e70bcd1572412e2b1856d1bcd1bbaa
lincolnteo <lincontmk@gmail.com>
2026-04-09 17:52:59 +0100

    installed dependancies

--- atlantic-horizon/client/vite.config.js
+++ atlantic-horizon/client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   build: {
 
     chunkSizeWarningLimit: 1000, // Raises the warning limit to 1MB
