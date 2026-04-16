644e45c9168599410e32f9a312077dc7e723d516
siewgeorge0920 <siewgeorge0920@gmail.com>
2026-04-16 14:09:15 +0100

    refactor: rename routes/index.js to routes/urlNameRoute.js

--- atlantic-horizon/server/v3/index.js
+++ atlantic-horizon/server/v3/index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import v3Router from './routes/index.js';
+import v3Router from './routes/urlNameRoute.js';
 import { AppError } from './utils/responseHandler.js';
 
 dotenv.config();
